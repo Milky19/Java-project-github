8ff760ec2dcbcb4ab81b3be8e8a747433484355c
krishna <krishna3@gmail.com>
2026-09-01 01:54:56 +0000

    Java-code-git

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+	public static void main(String[] args){
+		int a=10;
+		int b=5;
+		System.out.println("The sum is:"+(a+b));
+		System.out.println("The sub is:"+(a-b));
+		System.out.println("The mul is:"+(a*b));
+		System.out.println("The div is:"+(a/b));
+	}
+}
